aab49afc5b7f9bfb946adbf5fec9e0760d579309
ElFernan17 <ferx77silva2@gmail.com>
2026-07-07 13:43:12 -0500

    agregar el movimiento a la izquierda del personaje

--- limones.js
+++ limones.js
@@ -1,10 +1,21 @@
 let canvas = document.getElementById("areaJuego");
 let context = canvas.getContext("2d");
 
-let alturaSuelo = 350
-let alturaPersonaje = 30
-let anchoPersonaje = 15
+const alturaSuelo = 350
+const alturaPersonaje = 30
+const anchoPersonaje = 15
 
+let personajeX = canvas.width/2
+
+function limpiarCanva(){
+    context.clearRect(0,0,600,400)
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
 
 function iniciarEjecucion(){
     dibujarPersonaje();
@@ -14,10 +25,15 @@ function iniciarEjecucion(){
 
 function dibujarPersonaje(){
     context.fillStyle = "pink";
-    context.fillRect(canvas.width/2,320,anchoPersonaje,alturaPersonaje);
+    context.fillRect(personajeX,320,anchoPersonaje,alturaPersonaje);
 }
 
 function dibujarSuelo(){
     context.fillStyle = "green";
     context.fillRect(0,alturaSuelo,canvas.width,canvas.height);
+}
+
+function moverIzquierda(){
+    personajeX = personajeX - 10
+    actualizarPantalla();
 }
