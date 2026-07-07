07d6365c2fdca073c68b205ffecae39bd5873be0
ElFernan17 <ferx77silva2@gmail.com>
2026-07-07 13:29:46 -0500

    agregar personaje y suelo en funciones

--- limones.js
+++ limones.js
@@ -1,2 +1,23 @@
-let canvas = document.getElementById("areaJuego")
-let context = canvas.getContext("2d")
+let canvas = document.getElementById("areaJuego");
+let context = canvas.getContext("2d");
+
+let alturaSuelo = 350
+let alturaPersonaje = 30
+let anchoPersonaje = 15
+
+
+function iniciarEjecucion(){
+    dibujarPersonaje();
+
+    dibujarSuelo();
+}
+
+function dibujarPersonaje(){
+    context.fillStyle = "pink";
+    context.fillRect(canvas.width/2,320,anchoPersonaje,alturaPersonaje);
+}
+
+function dibujarSuelo(){
+    context.fillStyle = "green";
+    context.fillRect(0,alturaSuelo,canvas.width,canvas.height);
+}
