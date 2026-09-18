69fbe32b055008f8988f45db27b128edb94aad6f
ElFernan17 <ferx77silva2@gmail.com>
2026-09-18 09:41:39 -0500

    renombrar html

--- utilidades.js
+++ utilidades.js
@@ -12,3 +12,17 @@ function mostrarEnSpan(idSpan, textoSpan){
     let componente = document.getElementById("idSpan");
     componente.textContent = textoSpan;
 }
+
+function graficarRectangulo(x,y,ancho,alto,color){
+    let xFloat = parseFloat(x);
+    let yFloat = parseFloat(y);
+    let anchoFloat = parseFloat(ancho);
+    let altoFloat = parseFloat(alto);
+
+    context.fillStyle = color
+    context.fillRect(xFloat,yFloat,anchoFloat,altoFloat)
+}
+
+function numeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
